617821184aa8ae134ca5a20280a554a5d91257b1
Stepan <stepan.karelin.1996@mail.ru>
2024-07-03 23:10:06 +0700

    Basic auth

--- express/index.js
+++ express/index.js
@@ -1,45 +1,23 @@
-import express from "express";
-import nunjucks from "nunjucks";
-import { promises as fs } from "fs";
-import path from "path";
-import multer from "multer";
-import { fileURLToPath } from "url";
+const { configDotenv } = require("dotenv");
+const e = require("express");
+const basicAuth = require("express-basic-auth");
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+configDotenv()
 
-const app = express();
+const app = e();
 
-const uploadsDir = path.join(__dirname, "public", "uploads");
+app.use(
+  basicAuth({
+    realm: "Web.",
+    challenge: true,
+    users: {
+      admin: process.env.PASSWORD,
+    },
+  })
+);
 
-nunjucks.configure("views", {
-  autoescape: true,
-  express: app,
-});
-
-app.set("view engine", "njk");
-
-app.use(express.static("public"));
-
-const fileFilter = (req, file, cb) => {
-  cb(null, file.mimetype.match(/^image\//));
-};
-
-const upload = multer({
-  dest: uploadsDir,
-  fileFilter,
-  limits: {
-    fileSize: 10 * 1024 * 1024,
-  },
-});
-
-app.get("/", async (req, res) => {
-  const files = (await fs.readdir(uploadsDir)).sort().reverse();
-  res.render("index", { files });
-});
-
-app.post("/upload", upload.single("image"), (req, res) => {
-  res.redirect("/");
+app.get("/", (req, res) => {
+  res.send("Welcome");
 });
 
 const PORT = process.env.PORT || 3000;
