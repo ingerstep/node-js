00f0915fd85b23229f20559a5c15468660705be8
Stepan <stepan.karelin.1996@mail.ru>
2024-07-03 22:31:03 +0700

    Upload files local

--- express/index.js
+++ express/index.js
@@ -1,17 +1,49 @@
 import express from "express";
+import nunjucks from "nunjucks";
+import { promises as fs } from "fs";
+import path from "path";
+import multer from "multer";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(express.json());
+const uploadsDir = path.join(__dirname, "public", "uploads");
 
-app.use("/api/users", require("./users"));
-
-app.use((err, req, res, next) => {
-  res.status(500).send(err.message);
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
 });
 
-const PORT = process.env.PORT | 3000;
+app.set("view engine", "njk");
+
+app.use(express.static("public"));
+
+const fileFilter = (req, file, cb) => {
+  cb(null, file.mimetype.match(/^image\//));
+};
+
+const upload = multer({
+  dest: uploadsDir,
+  fileFilter,
+  limits: {
+    fileSize: 10 * 1024 * 1024,
+  },
+});
+
+app.get("/", async (req, res) => {
+  const files = (await fs.readdir(uploadsDir)).sort().reverse();
+  res.render("index", { files });
+});
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  res.redirect("/");
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Listening on port http://localhost:${PORT}`);
+  console.log(`Listening on http://localhost:${PORT}`);
 });
